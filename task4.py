e567cfac077ace64129b1f717f0764a962a943e4
Tanya <chebant@list.ru>
2020-10-31 19:05:58 +0300

    Задание 1.4

---
+++ task4.py
@@ -0,0 +1,20 @@
+# Пользователь вводит целое положительное число.
+# Найдите самую большую цифру в числе.
+# Для решения используйте цикл while и арифметические операции.
+
+def defineMaxNumber(input):
+    max = 0
+    step = 0
+
+    while step < len(input):
+        symbol = input[step]
+        num = int(symbol)
+        if (max < num): max = num
+        step = step + 1
+
+    return max
+
+if __name__ == '__main__':
+    input = input("Введите положительное число: ")
+    maxNumber = defineMaxNumber(input)
+    print(f'\n Max: {maxNumber}')
