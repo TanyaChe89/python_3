83a11a2c0c1ca7a36b23cddbeb959fe524d29a20
Tanya <chebant@list.ru>
2020-10-31 18:11:47 +0300

    Задание 1.1

---
+++ task1.py
@@ -0,0 +1,20 @@
+# Поработайте с переменными,
+# создайте несколько, выведите на экран,
+#
+# запросите у пользователя несколько чисел и
+# строк и сохраните в переменные, выведите на экран
+
+def print_variables():
+    x = ['2', 4]
+    z = True
+    y = 123
+    print(f'\n -> Simple variables: {x}; {y}; {z}')
+
+if __name__ == '__main__':
+    print_variables()
+    userVariable = input("Введите ваше имя: ")
+    print(f'Ваше имя - {userVariable}')
+    userVariable = input("Введите ваш возраст: ")
+    print(f'Ваш возраст - {userVariable}')
+    userVariable = input("True or False: ")
+    print({userVariable})
