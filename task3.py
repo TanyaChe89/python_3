ff7a10695721229130c4bd92e7940ecd493ad052
Tanya <chebant@list.ru>
2020-10-31 18:44:15 +0300

    Задание 1.3

---
+++ task3.py
@@ -0,0 +1,15 @@
+# Узнайте у пользователя число n.
+# Найдите сумму чисел n + nn + nnn.
+# Например, пользователь ввёл число 3.
+# Считаем 3 + 33 + 333 = 369.
+
+def convertAndCount(input):
+    num1 = int(input) # 3
+    num2 = int(input+input) # 33
+    num3 = int(input+input+input) # 333
+    return num1 + num2 + num3
+
+if __name__ == '__main__':
+    input = input("Введите n: ")
+    result = convertAndCount(input)
+    print(f'\n Result: {result}')
